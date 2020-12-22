f4d0841b6e2139ef868121ef27446506795fea7c
kryptocodes <srivatsantb@gmail.com>
2020-12-22 12:44:51 +0530

    Added Auth Entities: created User type Resolver: Register and Login Register added username and password validation
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,17 @@ import 'reflect-metadata'
 import { MikroORM } from "@mikro-orm/core"
 import { __prod__ } from "./constants"
 import microConfig from "./mikro-orm.config"
-import { Post } from './entities/Post'
 import express from 'express'
 import { ApolloServer } from 'apollo-server-express'
 import { buildSchema } from'type-graphql'
 import { HelloResolver } from "./resolvers/hello"
 import { PostResolver } from "./resolvers/post"
+import { UserResolver } from './resolvers/user'
+import redis from 'redis'
+import session from 'express-session'
+import connectRedis from 'connect-redis'
+
+
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
@@ -15,10 +20,21 @@ const main = async () => {
 
     const app = express()
    
+    const RedisStore = connectRedis(session)
+    const redisClient = redis.createClient()
+
+    app.use(
+    session({
+        name: "qid",
+        store: new RedisStore({ client: redisClient }),
+        secret: "idontknowwhybutitworks",
+        resave: false,
+    })
+    )
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [HelloResolver,PostResolver],
+            resolvers: [HelloResolver,PostResolver,UserResolver],
             validate: false
         }),
         context: () => ({ em: orm.em })
